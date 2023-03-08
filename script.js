c18980374b6d90af3f9dae381f739f01b5b07ba3
Omer Dai <109943178+omersdai-meeapps@users.noreply.github.com>
2023-03-08 16:35:41 +0300

    implement reset button to reset in the middle of the game

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const title = document.getElementById('title');
-const gameText = document.getElementById('gameText');
-const textInput = document.getElementById('textInput');
-const carEl = document.getElementById('car');
+const title = document.getElementById("title");
+const gameText = document.getElementById("gameText");
+const textInput = document.getElementById("textInput");
+const carEl = document.getElementById("car");
 
-const startBtn = document.getElementById('startBtn');
-const rollBtn = document.getElementById('rollBtn');
+const startBtn = document.getElementById("startBtn");
+const rollBtn = document.getElementById("rollBtn");
 
-const wpmEl = document.getElementById('wpm');
-const accuracyEl = document.getElementById('accuracy');
+const wpmEl = document.getElementById("wpm");
+const accuracyEl = document.getElementById("accuracy");
 
-const minuteEl = document.getElementById('minute');
-const secondEl = document.getElementById('second');
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
 
 textInput.onpaste = (e) => e.preventDefault(); // prevents pasting into input
-textInput.disabled = true;
 
 let { songName, artist, lyrics, link } = getRandomSong();
 
@@ -23,6 +22,7 @@ let inputLength;
 
 let words;
 let chars;
+let isGameOver;
 
 let correctPressCount;
 let incorrectPressCount;
@@ -33,33 +33,11 @@ let seconds;
 prepareGame();
 
 function prepareGame() {
-  charIndex = 0;
-  gameText.innerHTML = lyrics
-    .trim()
-    .split('\n')
-    .map((line) =>
-      line
-        .trim()
-        .split(' ')
-        .map(
-          (word) =>
-            `<span class="word">${word
-              .trim()
-              .split('')
-              .map(
-                (char) =>
-                  `<span class="char" index="${charIndex++}">${char}</span>`
-              )
-              .join(
-                ''
-              )}<span class="char" index="${charIndex++}"> </span></span>`
-        )
-        .join('')
-    )
-    .join('<br/>');
+  constructGameText();
 
-  words = gameText.querySelectorAll('.word');
-  chars = gameText.querySelectorAll('.char');
+  words = gameText.querySelectorAll(".word");
+  chars = gameText.querySelectorAll(".char");
+  isGameOver = false;
 
   wordIndex = 0;
   charIndex = 0;
@@ -68,29 +46,69 @@ function prepareGame() {
   incorrectPressCount = 0;
 
   toggleUnderLine(words[wordIndex]);
-  chars[charIndex].classList.toggle('active');
+  chars[charIndex].classList.toggle("active");
 
   // Reset DOM
-  wpmEl.innerText = '0';
-  accuracyEl.innerText = 'N/A';
-  minuteEl.innerText = '0';
-  secondEl.innerText = '00';
+  wpmEl.innerText = "0";
+  accuracyEl.innerText = "N/A";
+  minuteEl.innerText = "0";
+  secondEl.innerText = "00";
+
+  textInput.disabled = true;
+  textInput.innerHTML = "";
+  startBtn.disabled = false;
+  startBtn.innerText = "Start";
+  rollBtn.disabled = false;
+  rollBtn.innerText = "Roll Song";
 }
 
-startBtn.addEventListener('click', () => {
+function constructGameText() {
+  charIndex = 0;
+  gameText.innerHTML = lyrics
+    .trim()
+    .split("\n")
+    .map((line) =>
+      line
+        .trim()
+        .split(" ")
+        .map(
+          (word) =>
+            `<span class="word">${word
+              .trim()
+              .split("")
+              .map(
+                (char) =>
+                  `<span class="char" index="${charIndex++}">${char}</span>`
+              )
+              .join(
+                ""
+              )}<span class="char" index="${charIndex++}"> </span></span>`
+        )
+        .join("")
+    )
+    .join("<br/>");
+}
+
+startBtn.addEventListener("click", () => {
   prepareGame();
   startGame();
 });
 
-rollBtn.addEventListener('click', () => {
+rollBtn.addEventListener("click", () => {
+  if (!isGameOver) {
+    clearInterval(timer);
+    prepareGame();
+    return;
+  }
+
   ({ songName, artist, lyrics, link } = getRandomSong());
   prepareGame();
 });
 
-textInput.addEventListener('keydown', (e) => {
+textInput.addEventListener("keydown", (e) => {
   setTimeout(() => {
     // Add space if enter is pressed to improve UX
-    if (e.code === 'Enter') textInput.value += ' ';
+    if (e.code === "Enter") textInput.value += " ";
     let word = words[wordIndex];
     updateAccuracy();
 
@@ -99,7 +117,7 @@ textInput.addEventListener('keydown', (e) => {
       word.textContent === textInput.value.substring(0, word.textContent.length)
     ) {
       textInput.value = textInput.value.substring(word.textContent.length);
-      word.classList = 'word done';
+      word.classList = "word done";
       toggleUnderLine(word);
       if (++wordIndex === words.length) {
         stopGame();
@@ -116,7 +134,8 @@ textInput.addEventListener('keydown', (e) => {
 
 function startGame() {
   startBtn.disabled = true;
-  rollBtn.style.display = 'none';
+  rollBtn.disabled = true;
+  rollBtn.innerText = "Reset";
   let count = 3;
 
   startBtn.innerText = count;
@@ -129,8 +148,9 @@ function startGame() {
     } else {
       clearInterval(interval);
       textInput.disabled = false;
-      title.innerText = 'Type Racer';
-      startBtn.innerText = 'Type!';
+      title.innerText = "Type Racer";
+      startBtn.innerText = "Type!";
+      rollBtn.disabled = false;
       textInput.focus();
       startTimer();
     }
@@ -149,15 +169,17 @@ function updateClock() {
   minuteEl.innerText = parseInt(seconds / 60);
   secondEl.innerText = second < 10 ? `0${second}` : second;
 
-  console.log(wordIndex/words.length * 100);
-  carEl.style.transform = `translateX(${parseInt(wordIndex/words.length * 100)}%)`
+  console.log((wordIndex / words.length) * 100);
+  carEl.style.transform = `translateX(${parseInt(
+    (wordIndex / words.length) * 100
+  )}%)`;
 
   wpmEl.innerText = Math.round((wordIndex * 60) / seconds); // word per minute
   if (correctPressCount + incorrectPressCount > 0) {
     accuracyEl.innerText =
       parseInt(
         (correctPressCount * 100) / (correctPressCount + incorrectPressCount)
-      ) + '%';
+      ) + "%";
   }
 }
 
@@ -171,29 +193,32 @@ function stopGame() {
       Number(accuracyEl.innerText.substring(0, 2))
     }`
   );
+  isGameOver = true;
+
   textInput.disabled = true;
   startBtn.disabled = false;
-  startBtn.innerText = 'Start';
-  rollBtn.style.display = 'inline-block';
+  startBtn.innerText = "Start";
+  rollBtn.disabled = false;
+  rollBtn.innerText = "Roll Song";
 }
 
 function toggleUnderLine(word) {
   // You need to underline each character instead of the whole word to not underline the spaces
   word
-    .querySelectorAll('.char')
+    .querySelectorAll(".char")
     .forEach(
-      (char) => char.innerText === ' ' || char.classList.toggle('underline')
+      (char) => char.innerText === " " || char.classList.toggle("underline")
     );
 }
 
 function updateCharIndex() {
   try {
-    chars[charIndex].classList.remove('active');
+    chars[charIndex].classList.remove("active");
 
     charIndex =
-      parseInt(words[wordIndex].querySelector('.char').getAttribute('index')) +
+      parseInt(words[wordIndex].querySelector(".char").getAttribute("index")) +
       textInput.value.length;
-    chars[charIndex].classList.add('active');
+    chars[charIndex].classList.add("active");
   } catch (error) {
     // do nothing
   }
@@ -201,21 +226,21 @@ function updateCharIndex() {
 
 function updateWord(word) {
   gameText
-    .querySelectorAll('.incorrect')
-    .forEach((char) => char.classList.remove('incorrect'));
+    .querySelectorAll(".incorrect")
+    .forEach((char) => char.classList.remove("incorrect"));
   gameText
-    .querySelectorAll('.correct')
-    .forEach((char) => char.classList.remove('correct'));
+    .querySelectorAll(".correct")
+    .forEach((char) => char.classList.remove("correct"));
   const n = textInput.value.length;
-  const start = parseInt(word.querySelector('.char').getAttribute('index'));
+  const start = parseInt(word.querySelector(".char").getAttribute("index"));
   let flag = true;
   for (let i = 0; i < n; i++) {
     const char = chars[start + i];
     if (flag && textInput.value[i] === char.innerText) {
-      char.classList.add('correct');
+      char.classList.add("correct");
     } else {
       flag = false;
-      char.classList.add('incorrect');
+      char.classList.add("incorrect");
     }
   }
 }
@@ -236,6 +261,6 @@ function updateAccuracy() {
 }
 
 function getRandomSong() {
-  // return songs[2];
+  return songs[5];
   return songs[Math.floor(Math.random() * songs.length)];
 }
